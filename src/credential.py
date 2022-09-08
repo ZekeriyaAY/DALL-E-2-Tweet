4d6eb269968a14b77ae7de163514cc6d795e5e93
Zekeriya AY <zekeriya@zekeriyaay.com>
2022-09-08 22:51:19 +0300

    💄 Env Variables Renamed

--- src/credential.py
+++ src/credential.py
@@ -3,12 +3,12 @@ import os
 # usage: key = os.environ.get('API_Key')
 
 # Twitter API credentials
-consumer_key = os.environ.get('Twitter_Consumer_Key')
-consumer_secret = os.environ.get('Twitter_Consumer_Secret')
+consumer_key = os.environ.get('TWITTER_CONSUMER_KEY')
+consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET')
 
-access_token = os.environ.get('Twitter_Access_Token')
-access_token_secret = os.environ.get('Twitter_Access_Token_Secret')
+access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
+access_token_secret = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
 
 
 # DALL-E API credentials
-API_Key = os.environ.get('DALLE_API_Key')
+API_Key = os.environ.get('DALLE_API_KEY')
